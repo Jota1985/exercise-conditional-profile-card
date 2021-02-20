bc4e251cfef6b2f7bee49413f42fb55f73937050
jairosantamaria <jairosantamaria@amelearning.net>
2021-02-20 02:45:31 +0000

    conditional-profile-card

--- src/js/index.js
+++ src/js/index.js
@@ -27,16 +27,32 @@ function render(variables = {}) {
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  if (variables.name == null) {
+    variables.name = "Susan";
+  }
+  if (variables.lastname == null) {
+    variables.lastname = "Biolelt";
+  }
+  if (variables.city == null) {
+    variables.city = "Miami";
+  }
+  if (variables.country == null) {
+    variables.country = "USA";
+  }
+  if (variables.socialMediaPosition == null) {
+    variables.socialMediaPosition = "position-right";
+  }
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>${variables.name}</h1>
+          <h1>${variables.lastname}</h1>
           <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h3>${variables.city} ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
             <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
             <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
